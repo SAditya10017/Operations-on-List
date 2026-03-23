4a6d29e9fa2105e631e7a8bbf7e9e4a0657fe603
SAditya10017 <AdityaShankar.Suter@outlook.com>
2026-03-23 19:45:43 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+fav_fruits = ["water_apple", "watermelon", "honey_dew", "bread_fruit", "finger_lime"]
+print(len(fav_fruits))
+print(fav_fruits[0])
+print(fav_fruits[-1])
+print(fav_fruits[0:3])
+fav_fruits.pop(2)
+print(fav_fruits[0:3])
